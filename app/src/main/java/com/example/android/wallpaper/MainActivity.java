233b64c86799c9a16bc35b7ffe41be9678c32d56
Ahmed Elhendawy <ahm_elhen@yahoo.com>
2018-03-26 03:00:46 +0200

    change the main activity to be splash activity

--- app/src/main/java/com/example/android/wallpaper/MainActivity.java
+++ app/src/main/java/com/example/android/wallpaper/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.wallpaper;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//splash screen
+		
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+			startActivity(new Intent(MainActivity.this,HomeActivity.class));
+			finish();
+			}
+		},3000);// wait for 3 second
 	}
 }
